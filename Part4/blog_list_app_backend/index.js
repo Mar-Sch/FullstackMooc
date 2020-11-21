182581db60116b07e827b66a947a105d53d2e52c
schaamar <marco.schaafsma@tieto.com>
2020-11-21 21:14:30 +0200

    dotenv

--- Part4/blog_list_app_backend/index.js
+++ Part4/blog_list_app_backend/index.js
@@ -3,6 +3,7 @@ const express = require('express')
 const app = express()
 const cors = require('cors')
 const mongoose = require('mongoose')
+require('dotenv').config()
 
 const blogSchema = new mongoose.Schema({
     title: String,
@@ -13,7 +14,7 @@ const blogSchema = new mongoose.Schema({
 
 const Blog = mongoose.model('Blog', blogSchema)
 
-const mongoUrl = 'mongodb+srv://DB_User_14:User14Mooc@cluster0.jpoj8.mongodb.net/phonebook?retryWrites=true&w=majority'
+const mongoUrl = process.env.MONGODB_URI
 mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false, useCreateIndex: true })
 
 app.use(cors())
@@ -37,7 +38,8 @@ app.post('/api/blogs', (request, response) => {
         })
 })
 
-const PORT = 3003
+const PORT = process.env.PORT || 3003
+
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
 })
