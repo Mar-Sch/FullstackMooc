89dd917b0b114a6a1fc113eb126459221a79c150
schaamar <marco.schaafsma@tieto.com>
2020-11-22 09:17:27 +0200

    created a new controller file blogs.js

--- Part4/blog_list_app_backend/app.js
+++ Part4/blog_list_app_backend/app.js
@@ -3,7 +3,9 @@ const app = express()
 const cors = require('cors')
 const logger = require('./utils/logger')
 const mongoose = require('mongoose')
-const Blog = require('./models/blog')
+const blogsRouter = require('./controllers/blogs')
+
+
 
 require('dotenv').config()
 var morgan = require('morgan')
@@ -22,22 +24,6 @@ mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true, us
 app.use(cors())
 app.use(express.json())
 
-app.get('/api/blogs', (request, response) => {
-    Blog
-        .find({})
-        .then(blogs => {
-            response.json(blogs)
-        })
-})
-
-app.post('/api/blogs', (request, response) => {
-    const blog = new Blog(request.body)
-
-    blog
-        .save()
-        .then(result => {
-            response.status(201).json(result)
-        })
-})
+app.use('/api/blogs', blogsRouter)
 
 module.exports = app
