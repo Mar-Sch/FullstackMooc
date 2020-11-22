fca1e91a469048ff53920246ec5842dc3f3393ea
schaamar <marco.schaafsma@tieto.com>
2020-11-22 08:49:35 +0200

    added env variables to config file

--- Part4/blog_list_app_backend/index.js
+++ Part4/blog_list_app_backend/index.js
@@ -1,12 +1,13 @@
 const app = require('./app')
 const http = require('http')
 const logger = require('./utils/logger')
+const config = require('./utils/config')
 
 const server = http.createServer(app)
 
 
 const PORT = process.env.PORT || 3003
 
-server.listen(PORT, () => {
-    logger.info(`Server running on port ${PORT}`)
+server.listen(config.PORT, () => {
+    logger.info(`Server running on port ${config.PORT}`)
 })
