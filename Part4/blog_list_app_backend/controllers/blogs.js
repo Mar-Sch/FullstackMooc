202204f1fe9761c21cd7c99e9e6d8a3229854a09
schaamar <marco.schaafsma@tieto.com>
2020-11-28 13:27:29 +0200

    4.20 middleware

--- Part4/blog_list_app_backend/controllers/blogs.js
+++ Part4/blog_list_app_backend/controllers/blogs.js
@@ -3,6 +3,7 @@ const Blog = require('../models/blog')
 const User = require('../models/user')
 const jwt = require('jsonwebtoken')
 
+/* the below is now handled with middleware
 const getTokenFrom = request => {
     const authorization = request.get('authorization')
     if (authorization && authorization.toLowerCase().startsWith('bearer ')) {
@@ -10,6 +11,7 @@ const getTokenFrom = request => {
     }
     return null
 }
+*/
 
 blogsRouter.get('/', async (request, response) => {
     const blogs = await Blog
@@ -20,9 +22,9 @@ blogsRouter.get('/', async (request, response) => {
 blogsRouter.post('/', async (request, response) => {
     const body = request.body
 
-    const token = getTokenFrom(request)
-    const decodedToken = jwt.verify(token, process.env.SECRET)
-    if (!token || !decodedToken.id) {
+    
+    const decodedToken = jwt.verify(request.token, process.env.SECRET)
+    if (!request.token || !decodedToken.id) {
         return response.status(401).json({ error: 'token missing or invalid' })
     }
     const user = await User.findById(decodedToken.id)
@@ -47,7 +49,7 @@ blogsRouter.delete('/:id', async (request, response) => {
     response.status(204).end()
 })
 
-/*
+/* changed this to the async/await
 blogsRouter.put('/:id', (request, response, next) => {
     const body = request.body
 
