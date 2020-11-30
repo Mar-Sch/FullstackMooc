0d251681312aced272bab00b565953215f57517c
schaamar <marco.schaafsma@tieto.com>
2020-11-30 11:19:34 +0200

    Login details saved to local storage

--- Part5/bloglist-frontend/src/App.js
+++ Part5/bloglist-frontend/src/App.js
@@ -22,7 +22,10 @@ const App = () => {
                 username, password,
             })
             setUser(user)
-            console.log('Login successful')
+            console.log('Login successful', user)
+            window.localStorage.setItem(
+                'loggedBlogappUser', JSON.stringify(user)
+            )
             setUsername('')
             setPassword('')
         } catch (error) {
@@ -33,35 +36,52 @@ const App = () => {
         }
     }
 
+    const loginForm = () => (
+        <form onSubmit={handleLogin}>
+            <div>
+                username
+                    <input
+                    type="text"
+                    value={username}
+                    name="Username"
+                    onChange={({ target }) => setUsername(target.value)}
+                />
+            </div>
+            <div>
+                password
+                    <input
+                    type="password"
+                    value={password}
+                    name="Password"
+                    onChange={({ target }) => setPassword(target.value)}
+                />
+            </div>
+            <button type="submit">login</button>
+        </form>
+    )
+
+    const blogList = () => (
+        <div>
+            {
+                blogs.map(blog =>
+                    <Blog key={blog.id} blog={blog} />
+                )
+            }
+        </div>
+    )
+
     return (
         <div>
           <h2>blogs</h2>
 
-          <form onSubmit={handleLogin}>
-              <div>
-                  username
-            <input
-                      type="text"
-                      value={username}
-                      name="Username"
-                      onChange={({ target }) => setUsername(target.value)}
-                  />
-              </div>
-              <div>
-                  password
-            <input
-                      type="password"
-                      value={password}
-                      name="Password"
-                      onChange={({ target }) => setPassword(target.value)}
-                  />
-              </div>
-              <button type="submit">login</button>
-          </form>
+            {user === null ?
+                loginForm() :
+                <div>
+                    <p>{user.name} logged-in</p>
+                    {blogList()}
+                    </div>
+            }
 
-      {blogs.map(blog =>
-        <Blog key={blog.id} blog={blog} />
-      )}
     </div>
   )
 }
