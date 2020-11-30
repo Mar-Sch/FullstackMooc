de30e0be7ba7d1ca1d8031290981367809a9cece
schaamar <marco.schaafsma@tieto.com>
2020-11-30 10:32:14 +0200

    Login is working with console log for successfull login

--- Part5/bloglist-frontend/src/App.js
+++ Part5/bloglist-frontend/src/App.js
@@ -1,25 +1,40 @@
 import React, { useState, useEffect } from 'react'
 import Blog from './components/Blog'
 import blogService from './services/blogs'
+import loginService from './services/login'
 
 const App = () => {
     const [blogs, setBlogs] = useState([])
     const [username, setUsername] = useState('')
     const [password, setPassword] = useState('') 
+    const [user, setUser] = useState(null)
 
-  useEffect(() => {
-    blogService.getAll().then(blogs =>
-      setBlogs( blogs )
-    )  
-  }, [])
+    useEffect(() => {
+        blogService.getAll().then(blogs =>
+            setBlogs(blogs)
+        )
+    }, [])
 
-    const handleLogin = (event) => {
+    const handleLogin = async (event) => {
         event.preventDefault()
-        console.log('logging in with', username, password)
+        try {
+            const user = await loginService.login({
+                username, password,
+            })
+            setUser(user)
+            console.log('Login successful')
+            setUsername('')
+            setPassword('')
+        } catch (error) {
+            console.log('Wrong credentials')
+            setTimeout(() => {
+                console.log('timeout')
+            }, 5000)
+        }
     }
 
-  return (
-    <div>
+    return (
+        <div>
           <h2>blogs</h2>
 
           <form onSubmit={handleLogin}>
