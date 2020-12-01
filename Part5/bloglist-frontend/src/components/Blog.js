ddf7f3cdb2dd83a910945e5baa1623123a39c27e
schaamar <marco.schaafsma@tieto.com>
2020-12-01 10:56:56 +0200

    expand, collapse the blog item with extended data

--- Part5/bloglist-frontend/src/components/Blog.js
+++ Part5/bloglist-frontend/src/components/Blog.js
@@ -1,8 +1,35 @@
-import React from 'react'
-const Blog = ({ blog }) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>
-)
+import React, { useState } from 'react'
+
+const Blog = ({ blog }) => {
+    const blogStyle = {
+        paddingTop: 10,
+        paddingLeft: 2,
+        border: 'solid',
+        borderWidth: 1,
+        marginBottom: 5
+    }
+
+    const [visible, setVisible] = useState(false)
+
+    const hideWhenVisible = { display: visible ? 'none' : '' }
+    const showWhenVisible = { display: visible ? '' : 'none' }
+
+
+    return (
+        <div style={blogStyle}>
+            <div style={hideWhenVisible}>
+                {blog.title} {blog.author}
+                <button onClick={() => setVisible(true)}>Show</button>
+            </div>
+            <div style={showWhenVisible}>
+                <p>{blog.title} {blog.author}</p>
+                <p>{blog.url}</p>
+                <p>{blog.likes}</p>
+                <button onClick={() => setVisible(false)}>hide</button>
+            </div>
+        </div>
+    )
+}
+
 
 export default Blog
