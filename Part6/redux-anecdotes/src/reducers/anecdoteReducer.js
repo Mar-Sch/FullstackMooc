8c70c14ffeaff0120ec63039392695a1864e2352
schaamar <marco.schaafsma@tieto.com>
2020-12-22 15:30:34 +0200

    6.4 Adding new notes works

--- Part6/redux-anecdotes/src/reducers/anecdoteReducer.js
+++ Part6/redux-anecdotes/src/reducers/anecdoteReducer.js
@@ -24,6 +24,8 @@ const reducer = (state = initialState, action) => {
   console.log('action', action)
 
   switch(action.type) {
+    case 'NEW_ANECDOTE':
+      return [...state, action.data]
     case 'VOTE':
       const id = action.data.id
       const anecdoteToChange = state.find(n => n.id === id)
@@ -35,6 +37,20 @@ const reducer = (state = initialState, action) => {
       )
     default:
       return state
+  }
+}
+
+const generateId = () =>
+  Number((Math.random() * 1000000).toFixed(0))
+
+export const createAnecdote = (content) => {
+  return {
+    type: 'NEW_ANECDOTE',
+    data:{
+      content,
+      votes: 0,
+      id: generateId()
+    }
   }
 }
 
--- Part6/redux-anecdotes/src/reducers/anecdoteReducer.test.js
+++ Part6/redux-anecdotes/src/reducers/anecdoteReducer.test.js
@@ -2,18 +2,15 @@ import anecdoteReducer from './anecdoteReducer'
 import deepFreeze from 'deep-freeze'
 
 describe('anecdote reducer', () => {
-    const initialState = [
-            'anecdote 1',
-            'anecdote 2'
-        ]
+
   
-    test('should return a proper initial state when called with undefined state', () => {
+    test('should add a vote to anecdote', () => {
       const state = {}
       const action = {
         type: 'DO_NOTHING'
       }
   
       const newState = anecdoteReducer(undefined, action)
-      expect(newState).toEqual(initialState)
+      expect(newState).toEqual(anecdotesAtStart)
     })
 })
