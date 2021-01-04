dd86908fef4464aca1e0a64990cdd80bdac100ae
schaamar <marco.schaafsma@tieto.com>
2021-01-04 15:59:14 +0200

    Excise 7.2

--- Part7/routed-anecdotes/src/App.js
+++ Part7/routed-anecdotes/src/App.js
@@ -1,7 +1,7 @@
 import React, { useState } from 'react'
 import {
   BrowserRouter as Router,
-  Switch, Route, Link
+  Switch, Route, Link, useParams
 } from "react-router-dom"
 
 const Menu = () => {
@@ -29,6 +29,16 @@ const AnecdoteList = ({ anecdotes }) => (
     </ul>
   </div>
 )
+
+const Anecdote = ({anecdotes}) => {
+  const id = useParams().id
+  const anecdote = anecdotes.find(a => a.id === id)
+  return (
+    <div>
+      {anecdote.content}
+    </div>
+  )
+} 
 
 const About = () => (
   <div>
@@ -137,6 +147,9 @@ const App = () => {
         <Menu />
 
         <Switch>
+          <Route path="/anecdotes/:id">
+          <Anecdote anecdotes={anecdotes} />
+          </Route>
           <Route path="/anecdotes">
             <AnecdoteList anecdotes={anecdotes} />
           </Route>
